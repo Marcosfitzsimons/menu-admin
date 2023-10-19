b65e3a1f2e8df023de577d22b9555deda28a1389
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-10-18 22:55:12 -0300

    Update mobile dropdownmenu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,7 @@ import {
   LogOut,
   Map,
   Newspaper,
+  QrCode,
   User,
   Users,
 } from "lucide-react";
@@ -85,84 +86,22 @@ const Header = () => {
                     Panel de Control
                   </Link>
                 </DropdownMenuItem>
-                <DropdownMenuLabel className="uppercase text-black/80 font-bold text-sm dark:text-white">
-                  Listas
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator className="dark:bg-zinc-600" />
-                <DropdownMenuItem className="relative flex items-center gap-2 cursor-pointer p-0 hover:text-blue-lagoon-700">
-                  <Map className="absolute left-2 h-4 w-4 text-blue-lagoon-900/60 dark:text-blue-lagoon-300" />
-                  <Link
-                    to="/trips"
-                    className="rounded-lg py-1.5 z-20 pl-7 px-2 flex items-center gap-1 w-full text-start bg-transparent text-blue-lagoon-900 hover:bg-blue-lagoon-100/20 dark:text-blue-lagoon-50 dark:hover:text-white dark:hover:bg-zinc-700/20"
-                  >
-                    Viajes semanales
-                  </Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="relative flex items-center gap-2 cursor-pointer p-0 hover:text-blue-lagoon-700">
-                  <Map className="absolute left-2 h-4 w-4 text-blue-lagoon-900/60 dark:text-blue-lagoon-300" />
-                  <Link
-                    to="/special-trips"
-                    className="rounded-lg py-1.5 z-20 pl-7 px-2 flex items-center gap-1 w-full text-start bg-transparent text-blue-lagoon-900 hover:bg-blue-lagoon-100/20 dark:text-blue-lagoon-50 dark:hover:text-white dark:hover:bg-zinc-700/20"
-                  >
-                    Viajes particulares
-                  </Link>
-                </DropdownMenuItem>
                 <DropdownMenuItem className="cursor-pointer p-0">
-                  <Users className="absolute left-2 h-4 w-4 text-blue-lagoon-900/60 dark:text-blue-lagoon-300" />
+                  <QrCode className="absolute left-2 h-4 w-4 text-blue-lagoon-900/60 dark:text-blue-lagoon-300" />
                   <Link
-                    to="/users"
+                    to="/menu-qr"
                     className="rounded-lg py-1.5 z-20 pl-7 px-2 flex items-center gap-1 w-full text-start bg-transparent text-blue-lagoon-900 hover:bg-blue-lagoon-100/20 dark:text-blue-lagoon-50 dark:hover:text-white dark:hover:bg-zinc-700/20"
                   >
-                    Usuarios
+                    Menu QR
                   </Link>
                 </DropdownMenuItem>
-                <DropdownMenuItem className="cursor-pointer p-0">
-                  <Newspaper className="absolute left-2 h-4 w-4 text-blue-lagoon-900/60 dark:text-blue-lagoon-300" />
-                  <Link
-                    to="/publications"
-                    className="rounded-lg py-1.5 z-20 pl-7 px-2 flex items-center gap-1 w-full text-start bg-transparent text-blue-lagoon-900 hover:bg-blue-lagoon-100/20 dark:text-blue-lagoon-50 dark:hover:text-white dark:hover:bg-zinc-700/20"
-                  >
-                    Publicaciones
-                  </Link>
-                </DropdownMenuItem>
-                <DropdownMenuLabel className="uppercase text-black/80 font-bold text-sm dark:text-white">
-                  Ganancias
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator className="dark:bg-zinc-600" />
-                <DropdownMenuItem className="cursor-pointer p-0">
-                  <BarChart3 className="absolute left-2 h-4 w-4 text-blue-lagoon-900/60 dark:text-blue-lagoon-300" />
-                  <Link
-                    to="/yearly"
-                    className="rounded-lg py-1.5 z-20 pl-7 px-2 flex items-center gap-1 w-full text-start bg-transparent text-blue-lagoon-900 hover:bg-blue-lagoon-100/20 dark:text-blue-lagoon-50 dark:hover:text-white dark:hover:bg-zinc-700/20"
-                  >
-                    Resumen anual
-                  </Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="cursor-pointer p-0">
-                  <LineChart className="absolute left-2 h-4 w-4 text-blue-lagoon-900/60 dark:text-blue-lagoon-300" />
-                  <Link
-                    to="/monthly"
-                    className="rounded-lg py-1.5 z-20 pl-7 px-2 flex items-center gap-1 w-full text-start bg-transparent text-blue-lagoon-900 hover:bg-blue-lagoon-100/20 dark:text-blue-lagoon-50 dark:hover:text-white dark:hover:bg-zinc-700/20"
-                  >
-                    Resumen mensual
-                  </Link>
-                </DropdownMenuItem>
+
                 <DropdownMenuLabel className="uppercase text-black/80 font-bold text-sm dark:text-white">
                   Admin
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator className="dark:bg-zinc-600" />
                 <DropdownMenuItem className="cursor-pointer p-0">
-                  <User className="absolute left-2 h-4 w-4 text-blue-lagoon-900/60 dark:text-blue-lagoon-300" />
-                  <Link
-                    to="/mi-perfil"
-                    className="rounded-lg py-1.5 z-20 pl-7 px-2 flex items-center gap-1 w-full text-start bg-transparent text-blue-lagoon-900 hover:bg-blue-lagoon-100/20 dark:text-blue-lagoon-50 dark:hover:text-white dark:hover:bg-zinc-700/20"
-                  >
-                    Perfil
-                  </Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="cursor-pointer p-0">
                   <LogOut className="absolute left-2 h-4 w-4 text-blue-lagoon-900/60 dark:text-blue-lagoon-300" />
-
                   <button
                     onClick={handleLogOut}
                     className="rounded-lg py-1.5 z-20 pl-7 px-2 flex items-center gap-1 w-full text-start bg-transparent text-blue-lagoon-900 hover:bg-blue-lagoon-100/20 dark:text-blue-lagoon-50 dark:hover:text-white dark:hover:bg-zinc-700/20"
